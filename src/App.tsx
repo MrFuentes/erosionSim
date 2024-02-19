673a7aa6bb2e3fee57bc8c865395e486f9f2023c
MrFuentes <seangit99@gmail.com>
2024-02-19 12:27:19 +0100

    Add basic three demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import * as THREE from "three"
+import * as React from "react"
+import { Canvas , useFrame, ThreeElements } from "@react-three/fiber"
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Box(props: ThreeElements["mesh"]) {
+    const ref = React.useRef<THREE.Mesh>(null!);
+    const [hovered, hover] = React.useState(false);
+    const [clicked, click] = React.useState(false);
+    useFrame((state, delta) => (ref.current.rotation.x += delta));
+    return (
+        <mesh
+            {...props}
+            ref={ref}
+            scale={clicked ? 1.5 : 1}
+            onClick={(event) => click(!clicked)}
+            onPointerOver={(event) => hover(true)}
+            onPointerOut={(event) => hover(false)}
+        >
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? "hotpink" : "orange"}/>
+        </mesh>
+    );
 }
 
-export default App
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className="large">Hello World!</h1>
+                <Canvas>
+                    <ambientLight intensity={Math.PI/2}  />
+                    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+                    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+                    <Box position={[1.2, 0, 0]} />
+                    <Box position={[-1.2, 0, 0]} />
+                </Canvas>
+            </div>
+        )
+    }
+}
+
+export default App;
